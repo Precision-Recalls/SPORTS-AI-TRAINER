6733c37817959340c609dc3139eaee2c0f82764d
Garv Agarwal <garvagarwal72@gmail.com>
2024-06-27 08:32:49 +0530

    trained yolo8 s, m, n

--- test.py
+++ test.py
@@ -1,31 +1,28 @@
-from inference import get_model
-import supervision as sv
 import cv2
 from ultralytics import YOLO
-from inference import get_model
-
-#model = get_model(model_id="basketball-detection-dn6fg/4")
-
-#results = model.infer("https://media.roboflow.com/inference/people-walking.jpg")
 
 # Initialize the model
-model=YOLO("yolov8n.pt")
+model=YOLO("/Users/garvagarwal/Desktop/SPORTS-AI-TRAINER/best_yolo8m.pt")
 
-# Create supervision annotators
-bounding_box_annotator = sv.BoundingBoxAnnotator()
-label_annotator = sv.LabelAnnotator()
 
 def process_frame(frame):
-    # Run inference on the frame
-    results = model(frame)[0]
+    
+    results=model(frame,conf=0.8)
+    print(results)
     # Visualize the results on the frame
     annotated_frame = results[0].plot()
-    # Load the results into the supervision Detections API
-    #detections = sv.Detections.from_inference(results)
-
-    # Annotate the frame with bounding boxes and labels
-    #annotated_frame = bounding_box_annotator.annotate(scene=frame, detections=detections)
-    #annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections)
+   
+    # for result in results:
+    #     boxes = result.boxes  # Boxes object for bounding box outputs
+    #     masks = result.masks  # Masks object for segmentation masks outputs
+    #     keypoints = result.keypoints  # Keypoints object for pose outputs
+    #     probs = result.probs  # Probs object for classification outputs
+    #     obb = result.obb  # Oriented boxes object for OBB outputs
+        #result.show()  # display to screen
+        #result.save(filename="result.jpg")  # save to disk
+    # # Annotate the frame with bounding boxes and labels
+    # annotated_frame = bounding_box_annotator.annotate(scene=frame, detections=detections)
+    # annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections)
 
     return annotated_frame
 
@@ -40,7 +37,7 @@ def process_image(image_path):
     # Process and display the image
     annotated_image = process_frame(image)
     cv2.imshow('Annotated Image', annotated_image)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
 
 
@@ -61,7 +58,7 @@ def process_video(video_path):
 
         # Display the annotated frame
         cv2.imshow('Annotated Video', annotated_frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(10) & 0xFF == ord('q'):
             break
 
     # Release video capture and close OpenCV windows
@@ -75,5 +72,5 @@ def process_video(video_path):
 # process_image(image_path)
 
 # Process a video
-video_path = "/Users/garvagarwal/Desktop/SPORTS-AI-TRAINER/test/The player with no arc on their shot 😂🏀 #shorts.mp4"
+video_path = "/Users/garvagarwal/Desktop/SPORTS-AI-TRAINER/test/one_score_one_miss.mp4"
 process_video(video_path)
