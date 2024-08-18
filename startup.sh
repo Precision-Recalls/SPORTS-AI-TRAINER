4b9057087beab6ec9bcd8276b0ac7c5d10801649
Abhay Agarwal <abhayagarwal94@gmail.com>
2024-08-18 17:01:07 +0530

    port info updated

--- startup.sh
+++ startup.sh
@@ -2,7 +2,8 @@
 
 apt-get update && apt-get install -y libgl1-mesa-glx
 
+EXPOSE 5000
 
 # Start the Gunicorn server
-exec gunicorn --bind 0.0.0.0:8000 app:app
+exec gunicorn --bind 0.0.0.0:5000 app:app
 
