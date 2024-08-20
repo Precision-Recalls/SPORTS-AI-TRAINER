9c430adef56c61304031008a8cd659bfae3dd234
Abhay Agarwal <abhayagarwal94@gmail.com>
2024-08-20 16:54:38 +0530

    port changed

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 apt-get update && apt-get install -y libgl1-mesa-glx libglib2.0-0
 
-EXPOSE 5000
+EXPOSE 8000
 
 # Start the Gunicorn server
-exec gunicorn --bind 0.0.0.0:5000 app:app
+exec gunicorn --bind 0.0.0.0:8000 app:app
