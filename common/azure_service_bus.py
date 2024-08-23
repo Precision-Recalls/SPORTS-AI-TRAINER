44a476886071813cd5aa62d623fd91f4ae6c566c
Garv Agarwal <garvagarwal72@gmail.com>
2024-08-23 17:19:07 +0530

    service bus testing done

--- common/azure_service_bus.py
+++ common/azure_service_bus.py
@@ -1,15 +1,18 @@
 import logging
-
+from flask import jsonify
+import json
 from azure.servicebus import ServiceBusMessage
 from azure.servicebus.exceptions import ServiceBusError
-
 logger = logging.Logger('CRITICAL')
 
 
 def send_message_to_bus(sender, response, videoType, video_blob_name):
     try:
-        message = ServiceBusMessage(str(response))
-        message.application_properties = {"MessageType": videoType, "VideoID": video_blob_name}
+        json_response = json.dumps(response)
+        message = ServiceBusMessage(json_response)
+        message.application_properties = {"VideoType": videoType, "VideoID": video_blob_name}
         sender.send_messages(message)
     except ServiceBusError as e:
         logger.error(f"Failed to send message: {e}")
+    except AttributeError as e:
+        logger.error(f"Attribute error: {e}, possibly related to sender object initialization.")
